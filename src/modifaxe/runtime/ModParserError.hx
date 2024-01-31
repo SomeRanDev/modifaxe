705da0d90806c4b4b7fdf7a4156c42b3ca9f5357
Robert Borghese <28355157+SomeRanDev@users.noreply.github.com>
2024-01-31 02:48:16 -0500

    Remove periods from the end of error messages

--- src/modifaxe/runtime/ModParserError.hx
+++ src/modifaxe/runtime/ModParserError.hx
@@ -17,12 +17,12 @@ enum ModParserError {
 class ModParserErrorFunctions {
 	public static function getMessage(error: ModParserError) {
 		return switch(error) {
-			case UnexpectedChar(String.fromCharCode(_) => charString): 'Unexpected character $charString.';
-			case ExpectedChar(String.fromCharCode(_) => charString): 'Expected character $charString.';
-			case ExpectedIdentifier: 'Expected identifier.';
-			case ExpectedBool: 'Expected true or false.';
-			case ExpectedDigit: 'Expected number.';
-			case SectionShouldBeStartOfLine: 'Section should start at the beginning of the line.';
+			case UnexpectedChar(String.fromCharCode(_) => charString): 'Unexpected character $charString';
+			case ExpectedChar(String.fromCharCode(_) => charString): 'Expected character $charString';
+			case ExpectedIdentifier: 'Expected identifier';
+			case ExpectedBool: 'Expected true or false';
+			case ExpectedDigit: 'Expected number';
+			case SectionShouldBeStartOfLine: 'Section should start at the beginning of the line';
 		}
 	}
 }
